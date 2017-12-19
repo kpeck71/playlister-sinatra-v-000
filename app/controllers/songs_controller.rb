44b2d14229b6fa51ee4c67e983480b4320900711
Kristen Peck <kpeck71@gmail.com>
2017-12-19 01:10:01 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+require 'rack-flash'
+
+class SongsController < ApplicationController
+
+  use Rack::Flash
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/songs' do
+    @songs = Song.all
+
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    flash[:message] = "Successfully created song."
+    
+    redirect("/songs/#{@song.slug}")
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'/songs/show'
+  end
+
+end
